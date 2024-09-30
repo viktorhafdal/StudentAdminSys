e5c3f59b46daac80faeeae87e7cfbf98a06d385a
viktorhafdal <vgerlingsson@gmail.com>
2024-09-30 11:40:26 +0200

    Added validation

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,10 +1,18 @@
-﻿namespace StudentAdminSys.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentAdminSys.Models {
     public class Student {
+        [Required(ErrorMessage = "Please enter the student's name")]
         public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Please choose an education")]
         public int Education { get; set; }
+
+        [Required(ErrorMessage = "Please choose an option")]
         public int SemesterId { get; set; }
+
+        [Required(ErrorMessage = "Please enter the student's email")]
+        [EmailAddress]
         public string? Email {  get; set; }
-
-
     }
 }
